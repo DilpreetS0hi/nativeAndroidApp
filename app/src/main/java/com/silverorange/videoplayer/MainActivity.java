1933c4ca679b2cf8f3427bdd2dd8457ad937e080
DilpreetS0hi <sohidilpreet1999@gmail.com>
2022-07-19 18:29:03 -0700

    first commit

---
+++ app/src/main/java/com/silverorange/videoplayer/MainActivity.java
@@ -0,0 +1,11 @@
+package com.silverorange.videoplayer;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public final class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+    }
+}
